9c460fde4841b591d5585c28c067092e1c9df7db
yuyangtx062 <yuyangtx062@sogou-inc.com>
2021-02-01 11:04:03 +0800

    Initial Commit

---
+++ Clip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clip
+//
+//  Created by Robert Yu on 2/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
